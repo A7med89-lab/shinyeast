6b2e98b26229597e20a6dc4c17cecac942161b12
Ahmed <ahmed.shinyeast@gmail.com>
2025-05-09 18:17:38 +0300

    finished save data on purchases section on data_grid_view

---
+++ Dental/tamplate.aspx.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data.Sql;
+using System.Data;
+using System.Configuration;
+
+public partial class tamplate : System.Web.UI.Page
+{
+
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+           
+    }
+}
