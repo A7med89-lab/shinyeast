1a1578d61edd93c7a92a59c42896536e14d24bbf
A7med89-lab <tea.a.othmanabdo@gmail.com>
2025-06-11 20:16:30 +0300

    admit changes on purchase and store in and product cost

--- Dental/stock_in_out.aspx
+++ Dental/stock_in_out.aspx
@@ -5,13 +5,76 @@
 <asp:Content ID="Content2" runat="server" contentplaceholderid="PLACE_MASTER">
     <asp:Panel ID="PANEL_MASTER" runat="server">
     <br />
-    <asp:Label ID="Label1" runat="server" Text="Sales"></asp:Label>
+    <asp:Label ID="Label1" runat="server" Text="اوامر التوريد"></asp:Label>
     <dev>
         <br />
         <br />
+        <table class="auto-style10">
+                <tr>        
+                    <td>
+                        <asp:DropDownList ID="DRP_SUPP_FILTER" runat="server" AutoPostBack="true" OnSelectedIndexChanged="DRP_SUPP_FILTER_SelectedIndexChanged"></asp:DropDownList>
+                    </td>                    
+                    <td>
+                        <asp:TextBox ID="TXT_DATE_FILTER" runat="server" AutoPostBack="True" TextMode="Date" OnTextChanged="TXT_DATE_FILTER_TextChanged"></asp:TextBox>
+                    </td>                    
+                    <td>
+                        <asp:DropDownList ID="DRP_INV_FILTER" runat="server" AutoPostBack="true" OnSelectedIndexChanged="DRP_INV_FILTER_SelectedIndexChanged"> </asp:DropDownList>
+                    </td>
+                    <td>     
+                        <asp:TextBox ID="TXT_ORDER_FILTER" runat="server" AutoPostBack="true"></asp:TextBox>
+                    </td>
+                    <td>
+                        <asp:DropDownList ID="DRP_STATUS_FILTER" runat="server" AutoPostBack="True" OnSelectedIndexChanged="DRP_STATUS_FILTER_SelectedIndexChanged"></asp:DropDownList>
+                    </td>
+                    <td>
+                        &nbsp;</td>
+                </tr>
+            </table>
     </dev>
-    <asp:GridView ID="sales_grid" runat="server" CellPadding="4" ForeColor="#333333" GridLines="None" OnSelectedIndexChanged="sales_grid_SelectedIndexChanged">
+    <asp:GridView ID="PURCHASE_GRD" runat="server" CellPadding="4" ForeColor="#333333" GridLines="None" OnSelectedIndexChanged="sales_grid_SelectedIndexChanged" AutoGenerateColumns="False">
         <AlternatingRowStyle BackColor="White" />
+        <Columns>
+            <asp:TemplateField HeaderText="امر التوريد">
+                <ItemTemplate>
+                    <asp:Label ID="Label3" runat="server" Text='<%# Bind("id") %>'></asp:Label>
+                </ItemTemplate>
+            </asp:TemplateField>
+            <asp:TemplateField HeaderText="التاريخ">
+                 <ItemTemplate>
+                    <asp:Label ID="Label4" runat="server" Text='<%# Bind("date") %>'></asp:Label>
+                </ItemTemplate>
+            </asp:TemplateField>
+            <asp:TemplateField HeaderText="الوقت">
+                <ItemTemplate>
+                    <asp:Label ID="Label5" runat="server" Text='<%# Bind("time") %>'></asp:Label>
+                </ItemTemplate>
+            </asp:TemplateField>
+            <asp:TemplateField HeaderText="امر البيع">
+                <ItemTemplate>
+                    <asp:Label ID="Label6" runat="server" Text='<%# Bind("purchase_id") %>'></asp:Label>
+                </ItemTemplate>
+            </asp:TemplateField>
+            <asp:TemplateField HeaderText="المخزن">
+                <ItemTemplate>
+                    <asp:Label ID="Label7" runat="server" Text='<%# Bind("inv_name") %>'></asp:Label>
+                </ItemTemplate>
+            </asp:TemplateField>
+            <asp:TemplateField HeaderText="اجمالى الكمية">
+                <ItemTemplate>
+                    <asp:Label ID="Label8" runat="server" Text='<%# Bind("quantity") %>'></asp:Label>
+                </ItemTemplate>
+            </asp:TemplateField>
+            <asp:TemplateField HeaderText="المورد">
+                <ItemTemplate>
+                    <asp:Label ID="Labe19" runat="server" Text='<%# Bind("supplier_name") %>'></asp:Label>
+                </ItemTemplate>
+            </asp:TemplateField>
+            <asp:TemplateField HeaderText="الحالة (قبول\رفض)">
+                <ItemTemplate>
+                    <asp:Label ID="Labe20" runat="server" Text='<%# Bind("status") %>'></asp:Label>
+                </ItemTemplate>
+            </asp:TemplateField>
+        </Columns>
         <EditRowStyle BackColor="#2461BF" />
         <FooterStyle BackColor="#507CD1" Font-Bold="True" ForeColor="White" />
         <HeaderStyle BackColor="#507CD1" Font-Bold="True" ForeColor="White" />
@@ -25,10 +88,10 @@
     </asp:GridView>
     <br />
     <br />
-    <asp:Label ID="Label2" runat="server" Text="Purchase"></asp:Label>
+    <asp:Label ID="Label2" runat="server" Text="اوامر الصرف"></asp:Label>
     <br />
     <br />
-    <asp:GridView ID="purch_grid" runat="server" CellPadding="4" ForeColor="#333333" GridLines="None">
+    <asp:GridView ID="SALES_GRD" runat="server" CellPadding="4" ForeColor="#333333" GridLines="None">
         <AlternatingRowStyle BackColor="White" />
         <EditRowStyle BackColor="#2461BF" />
         <FooterStyle BackColor="#507CD1" Font-Bold="True" ForeColor="White" />
--- Dental/stock_in_out.aspx.cs
+++ Dental/stock_in_out.aspx.cs
@@ -12,17 +12,75 @@ using System.Configuration;
 
 public partial class stock_in_out : System.Web.UI.Page
 {
+    public void fill_inv()
+    {
+        string cst = "select id, name from inventories";
+        db.select_combo(cst, DRP_INV_FILTER);
+        DRP_INV_FILTER.Items.Insert(0, "اختيار المخزن");
+    }
+
+    public void fill_supp()
+    {
+        string cst = "select id, name from suppliers";
+        db.select_combo(cst, DRP_SUPP_FILTER);
+        DRP_SUPP_FILTER.Items.Insert(0, "اختيار المورد");
+    }
+
+    public void fill_status()
+    {
+        DRP_STATUS_FILTER.Items.Insert(0, "اختيار الحالة");
+        DRP_STATUS_FILTER.Items.Insert(1, "قبول");
+        DRP_STATUS_FILTER.Items.Insert(2, "رفض");
+    }
+
+    public void fill_grid_purchase_filter(string critria)
+    {
+        string select = "";
+        switch (critria)
+        {
+            case "inv":
+            select = "select stock_orders.id, stock_orders.[date], stock_orders.[time] , stock_orders.purchase_id,  inventories.name as inv_name, stock_order_details.quantity, suppliers.name as supplier_name, stock_orders.[status]  from stock_orders inner join  stock_order_details on stock_orders.id = stock_order_details.stock_order_id inner join inventories on stock_orders.inventory_id = inventories.id inner join suppliers on stock_orders.supplier_id = suppliers.id where stock_orders.[in]=1 and inventories.id = " + DRP_INV_FILTER.SelectedValue + "";
+            db.select(select, PURCHASE_GRD);
+            break;
+
+            case "supplier":
+            select = "select stock_orders.id, stock_orders.[date], stock_orders.[time] , stock_orders.purchase_id,  inventories.name as inv_name, stock_order_details.quantity, suppliers.name as supplier_name, stock_orders.[status]  from stock_orders inner join stock_order_details on stock_orders.id = stock_order_details.stock_order_id inner join inventories on stock_orders.inventory_id = inventories.id inner join suppliers on stock_orders.supplier_id = suppliers.id where stock_orders.[in] = 1 and suppliers.id = "+DRP_SUPP_FILTER.SelectedValue+"";
+            db.select(select, PURCHASE_GRD);
+            break;
+
+            case "permit":
+            select = "select stock_orders.id, stock_orders.[date], stock_orders.[time] , stock_orders.purchase_id,  inventories.name as inv_name, stock_order_details.quantity, suppliers.name as supplier_name, stock_orders.[status]  from stock_orders inner join  stock_order_details on stock_orders.id = stock_order_details.stock_order_id inner join inventories on stock_orders.inventory_id = inventories.id inner join suppliers on stock_orders.supplier_id = suppliers.id where stock_orders.[in]=1 and stock_orders.status=1";
+            db.select(select, PURCHASE_GRD);
+            break;
+
+            case "deny":
+            select = "select stock_orders.id, stock_orders.[date], stock_orders.[time] , stock_orders.purchase_id,  inventories.name as inv_name, stock_order_details.quantity, suppliers.name as supplier_name, stock_orders.[status]  from stock_orders inner join  stock_order_details on stock_orders.id = stock_order_details.stock_order_id inner join inventories on stock_orders.inventory_id = inventories.id inner join suppliers on stock_orders.supplier_id = suppliers.id where stock_orders.[in]=1 and stock_orders.status=0";
+            db.select(select, PURCHASE_GRD);
+            break;
+
+            case "date":
+           
+            select = "select stock_orders.id, stock_orders.[date], stock_orders.[time] , stock_orders.purchase_id,  inventories.name as inv_name, stock_order_details.quantity, suppliers.name as supplier_name, stock_orders.[status]  from stock_orders inner join  stock_order_details on stock_orders.id = stock_order_details.stock_order_id inner join inventories on stock_orders.inventory_id = inventories.id inner join suppliers on stock_orders.supplier_id = suppliers.id where stock_orders.[in]=1 and stock_orders.[date]='"+TXT_DATE_FILTER.Text+"'";
+            db.select(select, PURCHASE_GRD);
+            break;
+
+
+        }
+
+        
+    }
+
     public void fill_grid_sales()
     {
         string select = "select stock.id, stock.date, stock.sales_id , products.name as product, customers.name as customer , stock.quantity, inventory.name as inv_name from stock  inner join products on stock.product_id=products.id inner join customers on stock.customer_id = customers.id inner join inventory on stock.inventory_id = inventory.id  where stock.[out]=1";
-        db.select(select, sales_grid);
+        db.select(select, SALES_GRD);
         ViewState.Add("ds", db.ds1);
     }
 
     public void fill_grid_purchase()
     {
-        string select = "select stock.id, stock.date, stock.purchase_id , products.name as product, suppliers.name as supplier , stock.quantity, inventory.name as inv_name from stock  inner join products on stock.product_id=products.id inner join suppliers on stock.supplier_id = suppliers.id inner join inventory on stock.inventory_id = inventory.id  where stock.[in]=1";
-        db.select(select, purch_grid);
+        string select = "select stock_orders.id, stock_orders.[date], stock_orders.[time] , stock_orders.purchase_id,  inventories.name as inv_name, stock_order_details.quantity, suppliers.name as supplier_name, stock_orders.[status]  from stock_orders inner join  stock_order_details on stock_orders.id = stock_order_details.stock_order_id inner join inventories on stock_orders.inventory_id = inventories.id inner join suppliers on stock_orders.supplier_id = suppliers.id where stock_orders.[in]=1";
+        db.select(select, PURCHASE_GRD);
         ViewState.Add("ds", db.ds1);
     }
     database db = new database();
@@ -33,11 +91,72 @@ public partial class stock_in_out : System.Web.UI.Page
             fill_grid_purchase();
 
             fill_grid_sales();
+
+            fill_supp();
+            fill_inv();
+            fill_status();
         }
     }
 
     protected void sales_grid_SelectedIndexChanged(object sender, EventArgs e)
     {
 
+    }
+
+    protected void DRP_SUPP_FILTER_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        if (DRP_SUPP_FILTER.SelectedIndex !=0)
+        {
+            fill_grid_purchase_filter("supplier");
+        }
+        else
+        {
+            fill_grid_purchase();
+        }
+    }
+
+    protected void DRP_INV_FILTER_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        if (DRP_INV_FILTER.SelectedIndex != 0)
+        {
+            fill_grid_purchase_filter("inv");
+        }
+        else
+        {
+            fill_grid_purchase();
+        }
+    }
+
+    protected void DRP_STATUS_FILTER_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        if (DRP_STATUS_FILTER.SelectedIndex == 1)
+        {
+            fill_grid_purchase_filter("permit");
+        }
+        else if(DRP_STATUS_FILTER.SelectedIndex == 2)
+        {
+            fill_grid_purchase_filter("deny");
+        }
+        else
+        {
+            fill_grid_purchase();
+        }
+
+    }
+
+    protected void TXT_DATE_FILTER_TextChanged(object sender, EventArgs e)
+    {
+        if (string.IsNullOrWhiteSpace(TXT_DATE_FILTER.Text))
+        {
+            fill_grid_purchase();
+        }
+        else
+        {
+            fill_grid_purchase_filter("date");
+        }
+        
+
+        
+        
     }
 }
